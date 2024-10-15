7895dd2a1dd9dada0d82972df3deb0f71877372a
xiaruich <168495529+xiaruich@users.noreply.github.com>
2024-10-15 10:33:09 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,12 +1,11 @@
-alert('hello,xiarui');
+alert('Hello,xiarui!');
 alert(1+1);
 alert(Math.sqrt(1+1));
-alert(sqrt2+sqrt3);
+alert(Math.sqrt(2)+Math.sqrt(3));
 alert(1/0);
 alert(0/0);
 alert(2+3);
-alert(Math.sqrt('2'+'3'));
-alert(Math.sqrt('2')+Number('3'))
+alert('2'+'3');
+alert(Number('2')+Number('3'));
 alert('2'+3);
-alert(number('2')+3)
-
+alert(Number('2')+3);
