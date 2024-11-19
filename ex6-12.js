3bb39b61e4ef3ee5080d2bd658848127470ad917
xiaruich <168495529+xiaruich@users.noreply.github.com>
2024-11-19 10:05:22 +0900

    Update ex6-12.js

--- ex6-12.js
+++ ex6-12.js
@@ -1,1 +1,4 @@
 function hyoji(){//表示する文字var str ="一文字ずつ表示します．";//テキストボックスの文字数var cnt = document.timer.moji.value.length;//文字が全部表示されているか確認
+  if( con<11){//現在より1文字多く切り出して表示document.timer.moji.value=str.substr(0,cont+1);}
+    else{//全て表示されたら、空文字入力戻すdosument.timer.moji.value="";}}
+    function startnc(){//関数hyoji()を1000ミリ秒間隔で呼び出すsetlnterval("hoyji()",1000;}
