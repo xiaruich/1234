afbbaa6e500756545b53a98dbdf2d94279365afd
xiaruich <168495529+xiaruich@users.noreply.github.com>
2024-11-25 23:11:08 +0900

    Update ex06-102.js

--- ex06-102.js
+++ ex06-102.js
@@ -1,3 +1,3 @@
 function imgchange2(){document.getElementById('control-size').src='./image/keiei_small.jpg';}
                        
-function imgback2(){document.getElementById('control-size').src='./image/josai_new.png';}
+function imgback2(){document.getElementById('control-size').src='./image/josai.png';}
