25d8e9201cd2c92691a41dc23ed74dc2f4bdddc7
xiaruich <168495529+xiaruich@users.noreply.github.com>
2024-12-23 16:43:24 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,2 +1,2 @@
-function imgchange2(){document.getElementById('logo1').src='./image/bulboff.gif';}
-function imgmotosu2(){document.getElementById('logo1').src='./image/bulbon.gif';}
+function imgchange2(){document.getElementById('control-size').src='./image/bulboff.gif';}
+function imgmotosu2(){document.getElementById('control-size').src='./image/bulbon.gif';}
