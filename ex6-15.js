62ac1371e2957a1a4bff5ea78f9dd32b4707cb8c
xiaruich <168495529+xiaruich@users.noreply.github.com>
2025-01-07 10:05:11 +0900

    Update ex6-15.js

--- ex6-15.js
+++ ex6-15.js
@@ -1,2 +1,2 @@
-function imgchange3() {document.getElementById('logo3').src = './image/bulboff.gif';setTimeout('imgback3()', 3000);}
- function imgback3() {document.getElementById('logo3').src = './image/bulbon.gif';}
+function imgchange3() {document.getElementById('logo3').src= './image/bulbon.gif'; setTimeout('imgback3()',3000);}
+function imgback3() {document.getElementById('logo3').src='./image/bulboff.gif';}
